3dd65ac4f67bbd299208397a28478e9676714e1e
JanUrus <jama.urustomova.kk@gmail.com>
2022-02-13 20:48:43 +0600

    nft_card

---
+++ 2_week/js/script.js
@@ -0,0 +1,23 @@
+// let str = 'Hello world <br>';
+// document.write(str)
+
+// let hi = 'Hello world'
+// hi = 'Thank you for check <br>';
+// document.write(hi)
+
+// let name = 'Emirlan Jumaev';
+// let email = 'emirlan.jumaev.kk@gmail.com';
+// document.write(`<h1> ${name} </h1> <a email href="mailto:${email}"> ${email} </a>`)
+// let bezdelushek = +prompt('Сколько Безделушек ?');
+// let suvenir = +prompt('Сколько Сувенир ?');
+// let res = (bezdelushek * 112) + (suvenir * 75);
+// document.write(`<h3> Ваш пакет весит ${res}г </h3>`)
+let num1 = +prompt('Number 1');
+let num2 = +prompt('Number 2');
+
+document.write(` <br> <h3> Сумма a и b => ${num1 + num2} </h3>`)
+document.write(` <br> <h3> разница между a и b => ${num1 - num2} </h3>`)
+document.write(` <br> <h3> произведение a и b => ${num1 * num2} </h3>`)
+document.write(` <br> <h3> частное от деления a на b  => ${num1 / num2} </h3>`)
+document.write(` <br> <h3>  остаток от деления a на b  => ${num1 % num2} </h3>`)
+document.write(` <br> <h3>  результат возведения числа a в степень b  => ${num1 ** num2} </h3>`)
